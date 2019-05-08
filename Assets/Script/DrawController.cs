c76a9e8d6de626a383a6d6bc6f61555c89be4337
ccj <1019242086@qq.com>
2019-05-08 10:17:39 +0800

    init

---
+++ Assets/Script/DrawController.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawController : MonoBehaviour
+{
+
+    // Pencil对象
+    [SerializeField] GameObject Pencil;
+
+    // 新建描绘标记
+    public bool isPress;
+
+    // 新建pencils数组
+    List<GameObject> pencils = new List<GameObject>(0);
+
+    void Update()
+    {
+        //创建鼠标点击相机屏幕的位置，z轴为距离10
+        Vector3 mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10);
+
+        // 点击鼠标左键
+        if (Input.GetMouseButtonDown(0))
+        {
+            // 当鼠标左键按下后，允许描绘
+            isPress = true;
+            // Instantiate 实例化一个对象，就相当于复制/克隆一个对象
+            // 克隆GameObject或Component时，所有子对象和组件也将被克隆，其属性设置与原始对象的属性相同。
+            // Instantiate(Object original, Vector3 position, Quaternion rotation, Transform parent);
+            // Instantiate(需要复制对象, 新对象的位置, 新对象的方向, 将分配给新对象的父级);
+            // Quaternion.Euler返回围绕z轴旋转z度，围绕x轴旋转x度，围绕y轴旋转y度的旋转
+            // Camera.ScreenToWorldPoint :转变position从屏幕空间到世界空间。
+            pencils.Add(Instantiate(Pencil, Camera.main.ScreenToWorldPoint(mousePos), Quaternion.Euler(Vector3.zero), this.transform));
+        }
+        // 描绘标记判断
+        if (isPress)
+        {
+            //获取鼠标点击相机屏幕的坐标，设置当前描绘点的位置
+            pencils[pencils.Count - 1].transform.position = Camera.main.ScreenToWorldPoint(mousePos);
+
+        }
+        // 松开鼠标左键
+        if (Input.GetMouseButtonUp(0))
+        {
+            // 当鼠标左键按下后，禁止描绘
+            isPress = false;
+        }
+
+        // 点击键盘ESC键
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            //清除所有描绘的点
+            clearAllPencils();
+        }
+    }
+    
+
+    void clearAllPencils()
+    {
+
+        // 删除所有的点
+        for(int i =0; i < pencils.Count; i++)
+        {
+            Destroy(pencils[i]);
+        }
+        //清除数组
+        pencils.Clear();
+    }
+}
---
+++ Assets/Script/Loader.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+public class Loader : MonoBehaviour
+{
+    [SerializeField] string imgPath, settingPath;
+    [SerializeField] RawImage rawimage;
+    [SerializeField] RectTransform rawImageRect;
+
+    public bool changeImage, resizeImg;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(loadSetting());
+    }
+
+    IEnumerator loadSetting()//读取文档中的设定
+    {
+        UnityWebRequest ur = UnityWebRequest.Get(settingPath);
+        yield return ur.SendWebRequest(); //等待加载完成
+        if(ur.isHttpError || ur.isNetworkError)
+        {
+            Debug.Log(ur.error); //如果网络出现问题就会报错
+        }
+        else
+        {
+            string[] lines = ur.downloadHandler.text.Split('\n'); //用换行分割string
+            changeImage = bool.Parse(lines[0].Replace("changeImg:", "")); //去掉前面无用的文字
+            resizeImg  = bool.Parse(lines[1].Replace("resizeImg:", ""));
+
+        }
+        yield return loadImg(); //设定载入完成呼叫载入图片
+    }
+    IEnumerator loadImg()
+    {
+        UnityWebRequest ur = UnityWebRequestTexture.GetTexture(imgPath); //取texture要用UnityWebRequestTexture
+        yield return ur.SendWebRequest();  //等待加载完成
+        if (ur.isHttpError || ur.isNetworkError)
+        {
+            Debug.Log(ur.error); //如果网络出现问题就会报错
+        }
+        else
+        {
+            Texture loadTexture = ((DownloadHandlerTexture)ur.downloadHandler).texture;
+            if (changeImage)
+                rawimage.texture = loadTexture;
+
+            if (resizeImg)
+                rawImageRect.sizeDelta = new Vector2(loadTexture.width, loadTexture.height);
+
+        }
+    }
+}
