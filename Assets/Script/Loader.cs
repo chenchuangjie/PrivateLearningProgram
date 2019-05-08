b88a29360ed23c6bd711791518d7e85f67ec3cf5
ccj <1019242086@qq.com>
2019-05-08 13:58:17 +0800

    添加外部载入脚本注释和添加文件加载路径文件

--- Assets/Script/Loader.cs
+++ Assets/Script/Loader.cs
@@ -27,6 +27,7 @@ public class Loader : MonoBehaviour
         }
         else
         {
+            //downloadHandler 管理和处理从远程服务器接收的HTTP响应正文数据。
             string[] lines = ur.downloadHandler.text.Split('\n'); //用换行分割string
             changeImage = bool.Parse(lines[0].Replace("changeImg:", "")); //去掉前面无用的文字
             resizeImg  = bool.Parse(lines[1].Replace("resizeImg:", ""));
@@ -36,6 +37,8 @@ public class Loader : MonoBehaviour
     }
     IEnumerator loadImg()
     {
+        //UnityWebRequest 一个UnityWebRequest正确配置下载图像并将其转换为纹理。 注意：仅支持JPG和PNG格式。
+        //UnityWebRequestTexture.GetTexture 创建一个UnityWebRequest，用于通过HTTP GET下载图像，并根据检索到的数据创建一个Texture。
         UnityWebRequest ur = UnityWebRequestTexture.GetTexture(imgPath); //取texture要用UnityWebRequestTexture
         yield return ur.SendWebRequest();  //等待加载完成
         if (ur.isHttpError || ur.isNetworkError)
@@ -44,10 +47,15 @@ public class Loader : MonoBehaviour
         }
         else
         {
+            // 一个DownloadHandler子类，专门用于下载用作Texture对象的图像。
+            //DownloadHandlerTexture的texture属性：返回下载的纹理，或null。（只读）
+            // 管理和处理从远程服务器接收的HTTP响应正文数据。
             Texture loadTexture = ((DownloadHandlerTexture)ur.downloadHandler).texture;
+
+            // 设置贴图
             if (changeImage)
                 rawimage.texture = loadTexture;
-
+            //设置大小
             if (resizeImg)
                 rawImageRect.sizeDelta = new Vector2(loadTexture.width, loadTexture.height);
 
